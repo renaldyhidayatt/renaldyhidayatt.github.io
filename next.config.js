5a89a5c4a953e036f6e9dd5374f760c5f0f71ff3
holyraven <holyraven@pop-os.localdomain>
2023-08-27 15:59:38 +0700

    Adding

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  basePath: '/renaldyhidayatt.github.io',
+  basePath: '/github-pages',
   images: {
     unoptimized: true,
   },
