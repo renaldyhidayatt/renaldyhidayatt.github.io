404f655de53a4686abf6c088ea78a4eb869c786a
QuickHandle <renaldyhidayatt@gmail.com>
2025-02-05 15:13:03 +0700

    Initial Adding #26

--- content/blog/leetcode/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Pastikan folder exercism ada, jika tidak buat baru
-mkdir -p exercism
-
-# Pindahkan semua file/folder dengan prefix _exercism ke folder exercism
-mv _exercism* exercism/
-
-echo "Pemindahan selesai!"
